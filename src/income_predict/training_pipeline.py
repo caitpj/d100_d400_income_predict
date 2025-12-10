3cdfb11e8d7d7c38347e6a0f196f47f3472b63cd
Cai Parry-Jones <caiparryjones96@gmail.com>
2025-12-10 16:46:53 +0000

    update pipeline steps

--- src/income_predict/training_pipeline.py
+++ src/income_predict/training_pipeline.py
@@ -1,18 +1,18 @@
 import sys
 from pathlib import Path
 
-# import pandas as pd
+import pandas as pd
 
 current_file = Path(__file__).resolve()
 src_directory = current_file.parent.parent
 sys.path.append(str(src_directory))
 
-# from income_predict.cleaning import run_cleaning_pipeline
+from income_predict.cleaning import run_cleaning_pipeline
 from income_predict.evaluation import run_evaluation
-from income_predict.model_training import (  # run_split,
+from income_predict.model_training import (
     TARGET,
     load_training_outputs,
-    numeric_features,
+    run_split,
     run_training,
 )
 
@@ -20,11 +20,11 @@ print("Starting Pipeline...")
 
 # file_path = run_data_fetch_pipeline()
 # Seem to have been blocked from downloading from UCI repo, so using local file for now
-# file_path = src_directory / "data" / "census_income.parquet"
-# df_raw = pd.read_parquet(file_path)
-# run_cleaning_pipeline(df_raw)
+file_path = src_directory / "data" / "census_income.parquet"
+df_raw = pd.read_parquet(file_path)
+run_cleaning_pipeline(df_raw)
 
-# run_split()
+run_split()
 run_training()
 
 results = load_training_outputs()
@@ -33,7 +33,6 @@ run_evaluation(
     TARGET,
     results["glm_model"],
     results["lgbm_model"],
-    numeric_features,
     results["train_X"],
 )
 
